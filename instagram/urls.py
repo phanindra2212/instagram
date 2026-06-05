e44b2160e8e51584c61dda024ec28d9c6f58e5ff
phanindra2212 <phanindrayarajarla2005@gmail.com>
2026-06-05 14:40:07 +0530

    Remove duplicate path for instaapp.urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("", include("instaapp.urls")),
     path("admin/", admin.site.urls),
     path("instaapp/", include("instaapp.urls")),
+    path("", include("instaapp.urls")),
 ]
